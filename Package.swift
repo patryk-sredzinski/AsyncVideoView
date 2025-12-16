d283fd03b222e3299108b891244caf9e05447b55
Patryk Średziński <sredzinski.patryk@gmail.com>
2025-12-16 15:40:11 +0100

    Package root added

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version: 5.10
+
+import PackageDescription
+
+let package = Package(
+    name: "AsyncVideoView",
+    platforms: [
+        .iOS(.v16)
+    ],
+    products: [
+        .library(
+            name: "AsyncVideoView",
+            targets: ["AsyncVideoView"]
+        ),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/patryk-sredzinski/IteoLogger.git",
+            from: "1.7.0"
+        )
+    ],
+    targets: [
+        .target(
+            name: "AsyncVideoView",
+            dependencies: [
+                .product(name: "IteoLogger", package: "IteoLogger")
+            ],
+            path: "AsyncVideoView/Sources/AsyncVideoView"
+        ),
+    ]
+)
